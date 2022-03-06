26193131ab4119215456723da49a0fe63788228f
hellflame <hellflamedly@gmail.com>
2022-03-06 22:47:50 +0800

    update argparse

--- entry.go
+++ entry.go
@@ -6,7 +6,7 @@ import (
 )
 
 // Version info of the program
-const Version = "0.2.3"
+const Version = "0.2.4"
 
 func main() {
 	parser := argparse.NewParser("ipinfo", "tool for ip info lookup",
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/hellflame/ipinfo
 
 go 1.13
 
-require github.com/hellflame/argparse v1.7.0
+require github.com/hellflame/argparse v1.7.4
